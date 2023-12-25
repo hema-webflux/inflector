9d8c3eb3fe09e16c9f3788dffb434cc056c3ce59
xgbnl <745502274@qq.com>
2023-12-25 18:18:37 +0800

    docs

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Inflector
+
+**Simple example**
+```java
+import github.illuminate.Inflector;
+
+class GoodOrder {}
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public void test(){
+
+        String str = this.inflector.plural("GoodOrder"); // GoodOrders
+
+        str = this.inflector.snake(str,"_"); // good_orders
+
+    }
+
+}
+```
--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,11 +1,21 @@
+import github.illuminate.Inflector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestExample {
 
-    @Test
-    public void testEquals() {
-        Assertions.assertEquals("Hello", "hello");
+    private final Inflector inflector;
+
+    public TestExample(Inflector inflector) {
+        this.inflector = inflector;
+    }
+
+    public void test(){
+
+        String str = this.inflector.plural("GoodOrder"); // GoodOrders
+
+        str = this.inflector.snake(str,"_"); // good_orders
+
     }
 
 }

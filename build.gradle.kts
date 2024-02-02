c6d2133697c487f029ca8003435ef94e08cbf5d9
xgbnl <745502274@qq.com>
2024-02-02 15:22:32 +0800

    chore: 优化代码

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("java")
+    id("maven-publish")
+    signing
+}
+
+apply(from = findProperty("SIGNING_CONFIGURE"))
+
+group = findProperty("PACKAGE_GROUP") as String
+version = findProperty("PACKAGE_VERSION") as String
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            groupId = "${project.group}"
+            artifactId = project.name
+            version = "${project.version}"
+
+            from(components["java"])
+
+            pom {
+                name = project.name
+                description = property("POM_DESCRIPTION") as String
+                url = findProperty("POM_SCM_URL") as String
+
+                licenses {
+                    license {
+                        name = findProperty("POM_LICENSE_NAME") as String
+                        url = findProperty("POM_LICENSE_URL") as String
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = findProperty("POM_DEVELOPER_ID") as String
+                        name = findProperty("POM_DEVELOPER_NAME") as String
+                        email = findProperty("POM_DEVELOPER_EMAIL") as String
+                    }
+                }
+
+                scm {
+                    connection = findProperty("POM_SCM_CONNECTION") as String
+                    developerConnection = findProperty("POM_SCM_DEVELOPER_CONNECTION") as String
+                    url = findProperty("POM_SCM_URL") as String
+                }
+
+            }
+        }
+    }
+
+    repositories {
+        maven {
+
+            val isSnapshot = version.toString().endsWith("SNAPSHOT")
+
+            val repositories: String = if (isSnapshot) "SNAPSHOT" else "RELEASE"
+
+            url = uri(findProperty("NEXUS_${repositories}_URL") as String)
+
+            credentials {
+                username = project.ext.get("sonaUsername").toString()
+                password = project.ext.get("sonaPassword").toString()
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.2")
+}
